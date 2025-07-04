bfd3624835e95bbb3c150196679931b6f0298107
Maki <mcjohndraycabral@gmail.com>
2025-07-04 18:57:08 +0800

    add: taskcard and todo container

---
+++ src/components/taskCard.jsx
@@ -0,0 +1,10 @@
+function taskCard() {
+  return (
+    <div className="task-card">
+      <p>📝 Finish React project</p>
+      <button className="delete-btn">✖</button>
+  </div>
+  )
+}
+
+export default taskCard
