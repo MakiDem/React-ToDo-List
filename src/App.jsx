61c58703afbf1dbd82aa79fb77cfffa356d14269
Maki <mcjohndraycabral@gmail.com>
2025-07-05 15:37:43 +0800

    update: localStorage utilization

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import TaskCard from './components/taskCard.jsx'
 
 function App() {
   const [task, setTask] = useState("")
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const tasks = localStorage.getItem('tasks')
+    if (tasks) setTasks(JSON.parse(tasks))
+    console.log(tasks)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    console.log(tasks)
+  }, [tasks])
+
   const addTask = (e) => {
     e.preventDefault()
-    
+
     if (task.trim()) setTasks([...tasks, task])
       setTask('')
   }
